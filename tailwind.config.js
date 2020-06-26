e163af64e191c311c140eade493dc8286de9514d
S. Stottelaar <stottelaar.s@gmail.com>
2020-06-26 22:29:46 +0200

    Updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,14 @@ module.exports = {
   },
   variants: {},
   plugins: [],
-  purge: ['./components/**/*.vue', '.*.vue'],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js',
+    ],
+  },
 }
